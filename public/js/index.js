27442c468783894d64f8b94051ceef2a17e836db
xiaoli <1411389046@qq.com>
2018-08-30 08:42:34 +0800

    几个板块的布局已完成

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 function Index() {
-
+    this.addListener();
 }
+$.extend(Index.prototype, {
+    // 注册事件监听
+    addListener(){
+        $(".menu-list li").on("click", this.loadRightBox);
+    },
+    loadRightBox(){
+        $(".rightbox").eq($(this).index()).show().siblings().hide();
+    }
+});
 new Index();
